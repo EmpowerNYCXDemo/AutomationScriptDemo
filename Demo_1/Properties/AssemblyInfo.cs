3ebfb9dfbeab349433ed069860a5debf63fa20ad
GONZX018 <GONZX018@WX-015NW221101J>
2023-05-16 14:30:24 -0400

    Add project files.

---
+++ Demo_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Demo_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Demo_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("EC313154-DCE5-4A15-81A4-38710B0E5E79")]
+[assembly: AssemblyVersion("1.0.0.0")]
